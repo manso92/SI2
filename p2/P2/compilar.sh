d31bd90f82c71757be6a78166d356587e0db31cb
Pablo <92manso@gmail.com>
2017-03-21 17:53:07 +0100

    Creada la estructura de proyecto de la P2

---
+++ p2/P2/compilar.sh
@@ -0,0 +1,16 @@
+export J2EE_HOME=/usr/local/glassfish-4.1.1/glassfish/
+#!/bin/bash
+for i in P1-base P1-ws P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
+    cd $i
+    ant replegar;
+    ant delete-pool-local
+    cd -
+done
+cd P1-base
+ant delete-db
+cd -
+for i in P1-base P1-ejb-servidor-remoto P1-ejb-cliente-remoto P1-ws; do
+	cd $i
+	ant limpiar-todo todo
+	cd -
+done
